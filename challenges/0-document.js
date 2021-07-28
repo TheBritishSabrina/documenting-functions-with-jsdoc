b0976de122b68f6eb7e19d7027a9121072fac2eb
Sabrina Evans <sabrina.evans997@hotmail.co.uk>
2021-07-28 11:57:15 +0100

    Comment 0-document.js with JSDoc

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,4 +1,8 @@
-// TODO (Ex.0): document and write tests for this function
+/**
+ * < Return a negative version of a number >
+ * @param {number} n - input number
+ * @returns {number} negative version of `n`
+ */
 
 function asNegative(n) {
   // ...
